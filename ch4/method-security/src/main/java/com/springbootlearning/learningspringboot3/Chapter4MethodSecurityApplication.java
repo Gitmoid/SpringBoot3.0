cb4249b090b8ea1cac879502fcaf4c110082a833
Gitmoid <vrana.vojtech@post.cz>
2025-01-03 15:40:03 +0100

    Chapter 4 - App currently does not work, must finish the code.

--- ch4/method-security/src/main/java/com/springbootlearning/learningspringboot3/Chapter4MethodSecurityApplication.java
+++ ch4/method-security/src/main/java/com/springbootlearning/learningspringboot3/Chapter4MethodSecurityApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Chapter4ApplicationMethodSecurity {
+public class Chapter4MethodSecurityApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Chapter4ApplicationMethodSecurity.class, args);
+		SpringApplication.run(Chapter4MethodSecurityApplication.class, args);
 	}
 
 }
---
+++ ch4/oauth/src/main/java/com/springbootlearning/learningspringboot3/SearchListResponse.java
@@ -0,0 +1,4 @@
+package com.springbootlearning.learningspringboot3;
+
+record SearchListResponse(String kind, String etag, String nextPageToken, String prevPageToken, PageInfo pageInfo, SearchResult[] items) {
+}
