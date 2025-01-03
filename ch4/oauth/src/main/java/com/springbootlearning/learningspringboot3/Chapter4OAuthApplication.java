d39a6622f13888d152f9aa1abc93b6f340e22333
Gitmoid <vrana.vojtech@post.cz>
2025-01-03 15:42:35 +0100

    Chapter 4 - App currently does not work, must finish the code.

---
+++ ch4/oauth/src/main/java/com/springbootlearning/learningspringboot3/Chapter4OAuthApplication.java
@@ -0,0 +1,13 @@
+package com.springbootlearning.learningspringboot3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter4OAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chapter4OAuthApplication.class, args);
+	}
+
+}
